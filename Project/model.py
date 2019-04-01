a858f9ff65eb82d70c66f63da695a4e0fe606fde
sondrsag <sondre.sagstad@gmail.com>
2019-04-01 13:11:33 +0200

    virker

--- Project/model.py
+++ Project/model.py
@@ -2,7 +2,7 @@ import math
 from keras import models
 from keras import layers
 from keras import optimizers
-from keras.layers import Activation
+from keras.layers import Activation, Lambda, Reshape
 from dataloader import load_data
 import numpy as np
 
@@ -11,20 +11,43 @@ epochs=10
 batch_size=64
 
 [x_train, x_test, y_train, y_test] = load_data()
-x_val = x_train[-100];
-y_val = y_train[-100];
+
+
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+
+x_val = x_train[-100:];
+y_val = y_train[-100:];
 x_train = x_train[:-101];
 y_train = y_train[:-101];
-x_train = np.array(x_train)
+
+
+
+#x_val=x_train[-150:]
+#y_val=y_train[-150:]
+#print(x_train.shape)
+#print(x_val.shape)
+
+
+#x_train=x_train[:-150]
+#y_train=y_train[:-150]
+#print(x_train.shape)
 x_test = np.array(x_test)
-x_val = np.array(x_val)
+y_test = np.array(y_test)
+
+
+
+
 #Model
 input_shape=(160,320,3)
 
 model = models.Sequential()
 
+
+
 #Normalization
-model.add(layers.BatchNormalization(input_shape=input_shape))
+model.add(layers.BatchNormalization(input_shape=(160,320,3), name='norm'))
+
 
 #Convolutional layers
 model.add(layers.Conv2D(filters=24,kernel_size=(5,5), strides=(2,2), name='conv1'))
@@ -56,7 +79,9 @@ model.add(Activation('relu', name='relu8'))
 model.add(layers.Dense(units=1, activation='tanh'))
 
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-model.fit(x_train, y_train, batch_size = batch_size, epochs=epochs, validation_data=(x_val,y_val))
+print(model.summary())
+model.fit(x_train, y_train, steps_per_epoch=300, epochs=epochs,validation_steps=40 ,validation_data=(x_val,y_val))
 model.evaluate(x_test, y_test, batch_size=batch_size)
 
-model.fit_generator(train_generator, steps_per_epoch = ceil(num_train_samples / batch_size), epochs=epochs, valitation_data=val_generator,validation_steps= ceil(val_samples / batch_size))
+#model.fit_generator((x_train, y_train, steps_per_epoch = ceil(num_train_samples / batch_size),
+#epochs=epochs, valitation_data=(x_val,y_val),validation_steps= ceil(val_samples / batch_size)))
