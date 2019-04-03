93ef6d18638c154570ecd3a588396a8270f8fe47
audunhem <audunhem@gmail.com>
2019-04-03 23:44:04 +0200

    Relative file path

--- Project/dataloader.py
+++ Project/dataloader.py
@@ -5,12 +5,18 @@ import pandas as pd
 import os
 from random import shuffle
 import numpy as np
+import string
 
 
 def load_data():
     data_df = pd.read_csv(os.path.join(os.getcwd(),'recordings', 'driving_log.csv'), names=['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed'])
     X = data_df[['center', 'left', 'right']].values
     y = data_df['steering'].values
+    for x in X:
+        for i in range(3):
+            #folders = x[i].split("/") #when running on unix-generated files
+            folders = x[i].split("\\") #when using windows-generated files
+            x[i] = os.path.join(os.getcwd(),folders[-3],folders[-2],folders[-1])
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
     ind_list = [i for i in range(len(X_train))]
     shuffle(ind_list)
@@ -25,5 +31,3 @@ def load_data():
     for j in range(len(X_test)):
         x_test_vals.append(np.asarray(pyplot.imread(X_test[j][0])))
     return x_train_vals, x_test_vals, y_train, y_test
-
-#image, steering_angle = choose_image(data_dir, center, left, right, steering_angle)
