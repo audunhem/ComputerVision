f88b24c309b04add1af5599bbe1dfb4b9529f224
Audun <audunhem@gmail.com>
2019-04-04 15:22:41 +0200

    Update dataloader.py

--- Project/dataloader.py
+++ Project/dataloader.py
@@ -6,45 +6,67 @@ import os
 from random import shuffle
 import numpy as np
 import string
+import random
 
 
 def load_data():
+    random.seed()
     data_df = pd.read_csv(os.path.join(os.getcwd(),'recordings', 'driving_log.csv'), names=['center', 'left', 'right', 'steering', 'throttle', 'reverse', 'speed'])
     X = data_df[['center', 'left', 'right']].values
     y = data_df['steering'].values
     for x in X:
         for i in range(3):
-            folders = x[i].split("/") #when running on unix-generated files
+
+
+            folders = x[i].strip().split("/") #when running on unix-generated files
             #folders = x[i].split("\\") #when using windows-generated files
             x[i] = os.path.join(os.getcwd(),'recordings',folders[-2],folders[-1])
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-    ind_list = [i for i in range(len(X_train))]
-    shuffle(ind_list)
-    X_train  = X_train[ind_list]
-    y_train = y_train[ind_list]
+
+
     x_train_vals = [];
     x_test_vals = [];
-    y_train = np.asarray(y_train)
+    y_train_vals = [];
     y_test = np.asarray(y_test)
     right_turns = 0
     left_turns = 0
     straight = 0
     for i in range(len(X_train)):
-        if right_turns > left_turns and y_train[i] > 0:
-            x_train_vals.append(np.asarray(np.fliplr(pyplot.imread(X_train[i][0]))))
-            y_train[i] = -y_train[i]
-            left_turns = left_turns + 1
-        elif right_turns < left_turns and y_train[i] < 0:
-            x_train_vals.append(np.asarray(np.fliplr(pyplot.imread(X_train[i][0]))))
-            y_train[i] = -y_train[i]
-            right_turns = right_turns + 1
+        if random.random() > 0.2 and y_train[i] == 0:
+            #do nothing
+            continue
         else:
             x_train_vals.append(np.asarray(pyplot.imread(X_train[i][0])))
-            if y_train[i] < 0:
-                left_turns = left_turns + 1
-            elif y_train[i] > 0:
-                right_turns = right_turns + 1
-
+            y_train_vals.append(y_train[i])
+            x_train_vals.append(np.asarray(pyplot.imread(X_train[i][1])))
+            y_train_vals.append(y_train[i]+.25)
+            x_train_vals.append(np.asarray(pyplot.imread(X_train[i][2])))
+            y_train_vals.append(y_train[i]-.25)
     for j in range(len(X_test)):
         x_test_vals.append(np.asarray(pyplot.imread(X_test[j][0])))
-    return x_train_vals, x_test_vals, y_train, y_test
+    for i in range(len(X_train)):
+        if random.random() > 0.2 and y_train[i] == 0:
+            #do nothing
+            continue
+        else:
+            x_train_vals.append(np.asarray(np.fliplr(pyplot.imread(X_train[i][0]))))
+            y_train_vals.append(-y_train[i])
+            x_train_vals.append(np.asarray(np.fliplr(pyplot.imread(X_train[i][1]))))
+            y_train_vals.append(-(y_train[i]+.25))
+            x_train_vals.append(np.asarray(np.fliplr(pyplot.imread(X_train[i][2]))))
+            y_train_vals.append(-(y_train[i]-.25))
+
+    """
+    for i in range(len(X_train)):
+        if random.random() > 0.2 and y_train[i] == 0:
+            #do nothing
+            continue
+        else:
+            x_train_vals.append(np.asarray(np.fliplr(pyplot.imread(X_train[i][0]))))
+            y_train_vals.append(-y_train[i])
+    """
+    y_train_vals = np.asarray(y_train_vals)
+    c = list(zip(x_train_vals, y_train_vals))
+    random.shuffle(c)
+    x_train_vals, y_train_vals = zip(*c)
+    return x_train_vals, x_test_vals, y_train_vals, y_test
