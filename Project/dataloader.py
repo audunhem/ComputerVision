78b44209b2ed2a0d3c7b36496d912c8ba2bba592
Audun Gullikstad Hem <audungh@clab23.idi.ntnu.no>
2019-04-04 12:11:14 +0200

    flip

--- Project/dataloader.py
+++ Project/dataloader.py
@@ -14,20 +14,37 @@ def load_data():
     y = data_df['steering'].values
     for x in X:
         for i in range(3):
-            #folders = x[i].split("/") #when running on unix-generated files
-            folders = x[i].split("\\") #when using windows-generated files
-            x[i] = os.path.join(os.getcwd(),folders[-3],folders[-2],folders[-1])
+            folders = x[i].split("/") #when running on unix-generated files
+            #folders = x[i].split("\\") #when using windows-generated files
+            x[i] = os.path.join(os.getcwd(),'recordings',folders[-2],folders[-1])
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
     ind_list = [i for i in range(len(X_train))]
     shuffle(ind_list)
-    train_new  = X_train[ind_list]
-    target_new = y_train[ind_list]
+    X_train  = X_train[ind_list]
+    y_train = y_train[ind_list]
     x_train_vals = [];
     x_test_vals = [];
     y_train = np.asarray(y_train)
     y_test = np.asarray(y_test)
+    right_turns = 0
+    left_turns = 0
+    straight = 0
     for i in range(len(X_train)):
-        x_train_vals.append(np.asarray(pyplot.imread(X_train[i][0])))
+        if right_turns > left_turns and y_train[i] > 0:
+            x_train_vals.append(np.asarray(np.fliplr(pyplot.imread(X_train[i][0]))))
+            y_train[i] = -y_train[i]
+            left_turns = left_turns + 1
+        elif right_turns < left_turns and y_train[i] < 0:
+            x_train_vals.append(np.asarray(np.fliplr(pyplot.imread(X_train[i][0]))))
+            y_train[i] = -y_train[i]
+            right_turns = right_turns + 1
+        else:
+            x_train_vals.append(np.asarray(pyplot.imread(X_train[i][0])))
+            if y_train[i] < 0:
+                left_turns = left_turns + 1
+            elif y_train[i] > 0:
+                right_turns = right_turns + 1
+
     for j in range(len(X_test)):
         x_test_vals.append(np.asarray(pyplot.imread(X_test[j][0])))
     return x_train_vals, x_test_vals, y_train, y_test
