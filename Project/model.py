49da1f6bf36418121f6ad0fb2c31b46022d34d75
Audun <audunhem@gmail.com>
2019-04-05 17:12:51 +0200

    Update model.py
    Denne kjører greit

--- Project/model.py
+++ Project/model.py
@@ -2,7 +2,6 @@ import math
 from keras import models
 from keras import layers
 from keras import optimizers
-from keract import get_activations
 from keras.layers import Activation, Lambda, Reshape, Dropout
 from dataloader import load_data
 import numpy as np
@@ -14,7 +13,7 @@ model_filename="dave2.json"
 weights_filename="dave2.h5"
 
 #Hyper parameters
-epochs=10
+epochs=6
 batch_size=64
 
 [x_train, x_test, y_train, y_test] = load_data()
@@ -23,10 +22,10 @@ batch_size=64
 x_train = np.array(x_train)
 x_test = np.array(x_test)
 y_train = np.array(y_train)
-x_train=x_train[:,60:130,:,:]
+x_train=x_train[:,70:140,40:280,:]
 
 
-x_val = x_test[:,60:130,:,:];
+x_val = x_test[:,70:140,40:280,:];
 y_val = y_test;
 
 
@@ -43,10 +42,10 @@ x_test = np.array(x_test)
 y_test = np.array(y_test)
 
 
-datagen = ImageDataGenerator(brightness_range=[0.02, 3])
+datagen = ImageDataGenerator(brightness_range=[0.02, 2],height_shift_range=10,zca_whitening=True)
 
 #Model
-input_shape=(70,320,3)
+input_shape=(70,240,3)
 
 model = models.Sequential()
 
@@ -58,11 +57,11 @@ model.add(layers.BatchNormalization(input_shape=input_shape, name='norm'))
 #Convolutional layers
 model.add(layers.Conv2D(filters=24,kernel_size=(5,5), strides=(2,2), name='conv1'))
 model.add(Activation('relu', name='relu1'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 
 model.add(layers.Conv2D(filters=36, kernel_size=(5,5), strides=(2,2), name='conv2'))
 model.add(Activation('relu', name='relu2'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 
 model.add(layers.Conv2D(filters=48, kernel_size=(5,5), strides=(2,2), name='conv3'))
 model.add(Activation('relu', name='relu3'))
@@ -70,17 +69,17 @@ model.add(Dropout(0.5))
 
 model.add(layers.Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), name='conv4'))
 model.add(Activation('relu', name='relu4'))
-model.add(Dropout(0.1))
+#model.add(Dropout(0.1))
 
 model.add(layers.Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), name='conv5'))
 model.add(Activation('relu', name='relu5'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 
 #Fully connected layers
 model.add(layers.Flatten())
 model.add(layers.Dense(units=100, name='fc1'))
 model.add(Activation('relu', name='relu6'))
-model.add(Dropout(0.1))
+#model.add(Dropout(0.1))
 
 model.add(layers.Dense(units=50, name='fc2'))
 model.add(Activation('relu', name='relu7'))
@@ -88,7 +87,7 @@ model.add(Dropout(0.5))
 
 model.add(layers.Dense(units=10, name='fc3'))
 model.add(Activation('relu', name='relu8'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 
 model.add(layers.Dense(units=1, activation='tanh'))
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
