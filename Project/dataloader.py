0786e088bab21c4e5841b1d090a10b6a9d52eae5
Audun <audunhem@gmail.com>
2019-04-26 13:03:39 +0200

    Update dataloader.py

--- Project/dataloader.py
+++ Project/dataloader.py
@@ -9,6 +9,49 @@ import string
 import random
 import cv2
 
+def transform_incline(image, shift=(0.1,0.6), orientation='rand'):
+
+    rows,cols,ch = image.shape
+
+    vshift = random.uniform(shift[0],shift[1])
+    if orientation == 'rand':
+        orientation = random.choice(['down', 'up'])
+    if orientation == 'up':
+        vshift = -vshift*0.2
+    elif orientation != 'down':
+        raise ValueError("No or unknown orientation given. Possible values are 'up' and 'down'.")
+
+    dst = np.float32([[0., 0.], [1., 0.], [0., 1.], [1., 1.]]) * np.float32([cols, rows])
+
+    src = np.float32([[0., 0.], [1., 0.], [0-vshift, 1], [1+vshift, 1]]) * np.float32([cols, rows])
+    #Calculate the transformation matrix, perform the transformation,
+    #and return it.
+    M = cv2.getPerspectiveTransform(src, dst)
+    return cv2.warpPerspective(image, M, (cols, rows))
+
+
+def transform_curvature(image, shift=(0.1,0.8), orientation='rand'):
+
+    rows,cols,ch = image.shape
+
+    vshift = random.uniform(shift[0],shift[1])
+
+    if orientation == 'rand':
+        orientation = random.choice(['left', 'right'])
+
+    if orientation == 'left':
+        vshift = -vshift
+    elif orientation != 'right':
+        raise ValueError("No or unknown orientation given. Possible values are 'left' and 'right'.")
+
+    dst = np.float32([[0., 0.], [1., 0.], [0., 1.], [1., 1.]]) * np.float32([cols, rows])
+
+    src = np.float32([[0., 0.], [1., 0.], [0+vshift, 1], [1+vshift, 1]]) * np.float32([cols, rows])
+
+    #Calculate the transformation matrix, perform the transformation,
+    #and return it.
+    M = cv2.getPerspectiveTransform(src, dst)
+    return cv2.warpPerspective(image, M, (cols, rows)), vshift
 
 def load_data():
     random.seed()
@@ -20,6 +63,7 @@ def load_data():
 
 
             folders = x[i].strip().split("/") #when running on unix-generated files
+            #print(folders)
             #folders = x[i].split("\\") #when using windows-generated files
             x[i] = os.path.join(os.getcwd(),'recordings',folders[-2],folders[-1])
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
@@ -34,16 +78,24 @@ def load_data():
     straight = 0
     camera_offset= 0.3
     for i in range(len(X_train)):
-        if random.random() > 0.2 and y_train[i] == 0:
+        if random.random() > 0.3 and y_train[i] == 0:
             #do nothing
             continue
         else:
             x_train_vals.append(cv2.cvtColor(np.asarray(pyplot.imread(X_train[i][0])), cv2.COLOR_BGR2HSV))
             y_train_vals.append(y_train[i])
-            x_train_vals.append(cv2.cvtColor(np.asarray(pyplot.imread(X_train[i][1])), cv2.COLOR_BGR2HSV))
-            y_train_vals.append(y_train[i]+camera_offset)
-            x_train_vals.append(cv2.cvtColor(np.asarray(pyplot.imread(X_train[i][2])), cv2.COLOR_BGR2HSV))
-            y_train_vals.append(y_train[i]-camera_offset)
+            if random.random() > 0.4:
+                x_train_vals.append(cv2.cvtColor(np.asarray(pyplot.imread(X_train[i][1])), cv2.COLOR_BGR2HSV))
+                y_train_vals.append(y_train[i]+camera_offset)
+                x_train_vals.append(cv2.cvtColor(np.asarray(pyplot.imread(X_train[i][2])), cv2.COLOR_BGR2HSV))
+                y_train_vals.append(y_train[i]-camera_offset)
+            if(random.random() <= 0.2):
+                    x_train_vals.append(transform_incline(cv2.cvtColor(np.asarray(pyplot.imread(X_train[i][1])), cv2.COLOR_BGR2HSV)))
+                    y_train_vals.append(y_train[i])
+            if(random.random() <= 0.2):
+                    image, offset=transform_curvature(cv2.cvtColor(np.asarray(pyplot.imread(X_train[i][1])), cv2.COLOR_BGR2HSV))
+                    x_train_vals.append(image)
+                    y_train_vals.append(offset)
     for j in range(len(X_test)):
         x_test_vals.append(np.asarray(pyplot.imread(X_test[j][0])))
     for i in range(len(X_train)):
@@ -53,10 +105,11 @@ def load_data():
         else:
             x_train_vals.append(cv2.cvtColor(np.asarray(np.fliplr(pyplot.imread(X_train[i][0]))), cv2.COLOR_BGR2HSV))
             y_train_vals.append(-y_train[i])
-            x_train_vals.append(cv2.cvtColor(np.asarray(np.fliplr(pyplot.imread(X_train[i][1]))), cv2.COLOR_BGR2HSV))
-            y_train_vals.append(-(y_train[i]+camera_offset))
-            x_train_vals.append(cv2.cvtColor(np.asarray(np.fliplr(pyplot.imread(X_train[i][2]))), cv2.COLOR_BGR2HSV))
-            y_train_vals.append(-(y_train[i]-camera_offset))
+            if random.random() > 0.4:
+                x_train_vals.append(cv2.cvtColor(np.asarray(np.fliplr(pyplot.imread(X_train[i][1]))), cv2.COLOR_BGR2HSV))
+                y_train_vals.append(-(y_train[i]+camera_offset))
+                x_train_vals.append(cv2.cvtColor(np.asarray(np.fliplr(pyplot.imread(X_train[i][2]))), cv2.COLOR_BGR2HSV))
+                y_train_vals.append(-(y_train[i]-camera_offset))
 
     """
     for i in range(len(X_train)):
